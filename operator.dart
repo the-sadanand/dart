04b35579f0a52a3b0d103593f85811997473f559
the-sadanand <thesadanand.toponed0@gmail.com>
2025-09-05 18:04:37 +0530

    Airthematic operator

---
+++ operator.dart
@@ -0,0 +1,26 @@
+main() {
+  var operand1 = 10;
+  var operand2 = 7;
+
+  print(operand1 + operand2);
+  print(operand1 - operand2);
+  print(- operand1);
+  print(operand1 * operand2);
+  print(operand1 / operand2);
+  print(operand1 ~/ operand2);
+  print(operand1 % operand2);
+  var prefixIncrement = 5;
+
+  print(++prefixIncrement);
+   var postfixIncrement = 5;
+
+  print(postfixIncrement++);
+  print(postfixIncrement);
+   var prefixDecrement = 5;
+
+  print(--prefixDecrement);
+  var postfixDecrement = 5;
+
+  print(postfixDecrement--);
+  print(postfixDecrement);
+}
