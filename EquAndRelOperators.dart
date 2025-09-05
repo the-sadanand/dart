a927e82ca9cdbdf11c153428f23b392e897e62ef
the-sadanand <thesadanand.toponed0@gmail.com>
2025-09-05 18:09:25 +0530

    type Test and Relationl operators

---
+++ EquAndRelOperators.dart
@@ -0,0 +1,27 @@
+main() {
+  var operand1 = 10;
+  var operand2 = 7;
+
+  print(operand1 > operand2);
+  print(operand1 < operand2);
+  print(operand1 >= operand2);
+  print(operand1 <= operand2);
+  print(operand1 == operand2);
+  print(operand1 != operand2);
+    var operandA = 'a';
+  var operandB = 'b';
+
+  print(operandA == operandB);
+  print(operandA != operandB); 
+
+  double type1 = 5.0;
+  int type2 = 87;
+  String type3 = "educative";
+  bool type4 = true;
+
+  print(type1 is int);
+  print(type2 is int);
+  print(type3 is String);
+  print(type4 is double);
+  print(type4 is! double);
+}
